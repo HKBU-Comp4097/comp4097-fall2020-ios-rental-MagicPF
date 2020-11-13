6d63f594a605cdd465093055772e3f06be00dade
Pan Feng <1760495180@qq.com>
2020-11-14 02:48:41 +0800

    CoreData

---
+++ DataController.swift
@@ -0,0 +1,32 @@
+//
+//  DataController.swift
+//  Estate Rental
+//
+//  Created by 潘丰 on 14/11/2020.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    var persistentContainer: NSPersistentContainer
+    
+    init(completion: @escaping () -> ()) {
+        
+        persistentContainer = NSPersistentContainer(name: "EstateRentalModel")
+        persistentContainer.loadPersistentStores { (description, error) in
+            
+            if let error = error {
+                fatalError("Core Data stack could not be loaded. \(error)")
+            }
+            
+            // Called once initialization of Core Data stack is complete
+            DispatchQueue.main.async {
+                self.seedData()
+                completion()
+            }
+        }
+    }
+    
+}
---
+++ PropertyManangedObject+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PropertyManangedObject+CoreDataClass.swift
+//  Estate Rental
+//
+//  Created by 潘丰 on 14/11/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PropertyManangedObject)
+public class PropertyManangedObject: NSManagedObject {
+
+}
